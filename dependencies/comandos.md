e4f25fc86f58eaeeb07869c1bb0dce304ff45000
Juan Pablo Pineda Cortez <al2223033021@azc.uam.mx>
2026-07-05 15:43:44 -0600

    Agrega vistas publicas/privadas, nuevas rutas y navbar en header

---
+++ dependencies/comandos.md
@@ -0,0 +1,16 @@
+En el CMD entrar a la ruta donde esta guardado el proyecto
+    cd 'ruta'
+
+Ejecutar los siguientes comandos
+    node -v
+    npm init -y
+    npm install -g nodemon
+    npm install express
+    npm install mongoose
+    npm install ejs
+    nodemon servidor.js
+
+Deben tener instalado Node.js en su PC 
+Si todo salio bien entren en su navegador a:
+    localhost:3000
+    localhost:3000/404
--- router/rutasPagina.js
+++ router/rutasPagina.js
@@ -3,7 +3,171 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,resp) => {
-    resp.send('Pagina de inicio')
-})
+    resp.render('index', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/inicio', (req,resp) => {
+    resp.render('index', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/login', (req,resp) => {
+    resp.render('public/login', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/inicio-sesion', (req,resp) => {
+    resp.render('public/login', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/register', (req,resp) => {
+    resp.render('public/register', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/registro', (req,resp) => {
+    resp.render('public/register', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/cards', (req,resp) => {
+    resp.render('public/cards', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/tarjetas', (req,resp) => {
+    resp.render('public/cards', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/catalog', (req,resp) => {
+    resp.render('public/catalog', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/catalogo', (req,resp) => {
+    resp.render('public/catalog', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/account', (req,resp) => {
+    resp.render('private/account', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/cuenta', (req,resp) => {
+    resp.render('private/account', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/favorites', (req,resp) => {
+    resp.render('private/dashboard/favorites', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/favoritos', (req,resp) => {
+    resp.render('private/dashboard/favorites', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/fines', (req,resp) => {
+    resp.render('private/dashboard/fines', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/multas', (req,resp) => {
+    resp.render('private/dashboard/fines', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/loans', (req,resp) => {
+    resp.render('private/dashboard/loans', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/prestamos', (req,resp) => {
+    resp.render('private/dashboard/loans', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/notifications', (req,resp) => {
+    resp.render('private/dashboard/notifications', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/notificaciones', (req,resp) => {
+    resp.render('private/dashboard/notifications', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/profile', (req,resp) => {
+    resp.render('private/dashboard/profile', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/perfil', (req,resp) => {
+    resp.render('private/dashboard/profile', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/reservations', (req,resp) => {
+    resp.render('private/dashboard/reservations', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
+
+router.get('/reservas', (req,resp) => {
+    resp.render('private/dashboard/reservations', {
+        usuario: "Juan",
+        apellido: "Cortez"
+    })
+});
 
 module.exports = router;
